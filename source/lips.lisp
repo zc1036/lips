6cfd2faae722212cdea713d94f8713d174b36832
Zach Clark <zach@eq>
2016-01-07 05:08:33 -0600

    A few features
    Support for between-paragraph characters, post-process hooks, and
    SETF-Q (setf with no return value).

--- source/lips.lisp
+++ source/lips.lisp
@@ -23,50 +23,75 @@
 
 (defparameter *original-readtable* *readtable*)
 
+(defun princ-if (x)
+    (let ((res (etypecase x
+                 (function (funcall x))
+                 (t x))))
+        (when res (princ res))))
+
+(defparameter *paragraph-separator* nil)
+
 ;; Read characters until we get to a lisp form.
 (defun read-lips-char (stream read-char)
     ;; We print to an in-memory buffer to avoid the inefficiency of
     ;; printing one character at a time.
     (princ (with-output-to-string (stdout)
-             (let ((*standard-output* stdout))
+             (let ((*standard-output* stdout)
+                   (last-char-was-newline nil))
                  ;; Loop until there are no more characters in the
                  ;; input stream.
                  (loop
                     for char = read-char then (read-char stream nil nil t)
                     while char
                     do
+                      (when (not (eq char #\newline))
+                          (setf last-char-was-newline nil))
                       ;; If the character is *hot-char*, then check if
                       ;; it's escaped by peeking at the next character
                       ;; in the stream. If not, just print it. If so,
                       ;; go on.
-                      (if (eq char *hot-char*)
-                          ;; If it is escaped, then just print
-                          ;; *hot-char* and discard the extra one. ;;
-                          ;; Otherwise, read an object, macroexpand
-                          ;; and evaluate it, and if it's not nil,
-                          ;; print it.
-                          (if (eq (peek-char nil stream t nil t) *hot-char*)
-                              (progn
-                                  (princ *hot-char*)
-                                  (read-char stream nil nil t))
+                      (cond
+                        ((eq char *hot-char*)
+                         ;; If it is escaped, then just print
+                         ;; *hot-char* and discard the extra one. ;;
+                         ;; Otherwise, read an object, macroexpand
+                         ;; and evaluate it, and if it's not nil,
+                         ;; print it.
+                         (if (eq (peek-char nil stream t nil t) *hot-char*)
+                             (progn
+                                 (princ *hot-char*)
+                                 (read-char stream nil nil t))
 
-                              (let* ((*readtable* *original-readtable*)
-                                     (*package* (find-package :lips))
-                                     (obj (eval (macroexpand (read stream)))))
-                                  (when (not (null obj))
-                                      (princ obj))))
-
-                          (princ char)))))))
+                             (let* ((*readtable* *original-readtable*)
+                                    (*package* (find-package :lips))
+                                    (obj (eval (macroexpand (read stream)))))
+                                 (when (not (null obj))
+                                     (princ obj)))))
+                        ((eq char #\newline)
+                         (princ char)
+                         (when last-char-was-newline
+                             (princ-if *paragraph-separator*))
+                         (setf last-char-was-newline (not last-char-was-newline)))
+                        (t
+                         (setf last-char-was-newline nil)
+                         (princ char))))))))
 
 (defparameter lips-readtable (copy-readtable *readtable*))
 
 (loop for char in *characters* do
      (set-macro-character char #'read-lips-char nil lips-readtable))
 
+(defparameter *finish-hooks* nil)
+
+(defun add-finish-hook (func)
+    (push func *finish-hooks*)
+    (values))
+
 (defun main ()
     (let ((*readtable* lips-readtable))
         ;; (with-open-file (input (cadr command-line-args) :direction :input))
-        (loop while (read *standard-input* nil))))
+        (loop while (read *standard-input* nil))
+        (mapc #'princ-if *finish-hooks*)))
 
 ;;; Functions for use in text to be processed.
 
@@ -82,6 +107,10 @@
          (defparameter ,name ,value)
          (values)))
 
+;; Setf quietly
+(defmacro setf-q (place value)
+    `(progn (setf ,place ,value) (values)))
+
 ;; Loads the file as lisp source code, that is, without requiring the
 ;; *hot-char* to evaluate lisp.
 (defun include-defs (filename)
@@ -94,4 +123,3 @@
     (let ((*readtable* lips-readtable))
         (include-defs filename))
     (values))
-
